042c2bfa0798819c97768eea8eb0c0808870d1de
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-11-03 16:59:40 -0600

    added properties, constructor, getters, setters to merch class

--- src/main/java/com/projects/fullstackproject/controllers/Merch.java
+++ src/main/java/com/projects/fullstackproject/controllers/Merch.java
@@ -1,4 +1,99 @@
 package com.projects.fullstackproject.controllers;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "merch")
 public class Merch {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 20, nullable = false, unique = true)
+    private String name;
+
+    @Column(columnDefinition = "TEXT NOT NULL")
+    private String description;
+
+    @Column(length = 20, nullable = false)
+    private String price;
+
+    @Column(length = 50, nullable = false)
+    private String productCode;
+
+    @Column(length = 20, nullable = false)
+    private String productType;
+
+    @Column(length = 30)
+    private String productDetails;
+
+    @Column(length = 30)
+    private String shipping;
+
+    public Merch(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public String getProductDetails() {
+        return productDetails;
+    }
+
+    public void setProductDetails(String productDetails) {
+        this.productDetails = productDetails;
+    }
+
+    public String getShipping() {
+        return shipping;
+    }
+
+    public void setShipping(String shipping) {
+        this.shipping = shipping;
+    }
 }
