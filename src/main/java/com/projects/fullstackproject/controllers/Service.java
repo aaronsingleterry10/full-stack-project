7ceecd07a1b7483ec14c5e20b0360c7f114d044e
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-11-04 21:10:06 -0600

    added getters/setters

--- src/main/java/com/projects/fullstackproject/controllers/Service.java
+++ src/main/java/com/projects/fullstackproject/controllers/Service.java
@@ -16,4 +16,30 @@ public class Service {
 
     @Column(length = 20, nullable = false)
     private String serviceType;
+
+    public Service(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getServiceType() {
+        return serviceType;
+    }
+
+    public void setServiceType(String serviceType) {
+        this.serviceType = serviceType;
+    }
 }
