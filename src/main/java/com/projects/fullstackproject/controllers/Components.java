edfef4cf3cf3d04a591682521e8b5d7a056fd7c3
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-11-05 11:28:04 -0600

    added properties to components class

--- src/main/java/com/projects/fullstackproject/controllers/Components.java
+++ src/main/java/com/projects/fullstackproject/controllers/Components.java
@@ -1,4 +1,34 @@
 package com.projects.fullstackproject.controllers;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "components")
 public class Components {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 20, nullable = false)
+    private String componentName;
+
+    @Column(columnDefinition = "TEXT")
+    private String componentDescription;
+
+    @Column(length = 30)
+    private String componentDetails;
+
+    @Column(length = 30, nullable = false)
+    private String componentProductCode;
+
+    @Column(length = 20, nullable = false)
+    private String componentPrice;
+
+    @Column(length = 20, nullable = false)
+    private String componentType;
+
+    @Column(length = 20)
+    private String shipping;
+
 }
