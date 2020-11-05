6addf6e5b8863768370d0bc295eb3c69550f0a19
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-11-04 21:09:30 -0600

    added properties

--- src/main/java/com/projects/fullstackproject/controllers/Service.java
+++ src/main/java/com/projects/fullstackproject/controllers/Service.java
@@ -1,4 +1,19 @@
 package com.projects.fullstackproject.controllers;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "service")
 public class Service {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 20, nullable = false)
+    private String name;
+
+    @Column(length = 20, nullable = false)
+    private String serviceType;
 }
