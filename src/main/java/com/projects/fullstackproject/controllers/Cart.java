19844c493cf47571a3702a2339484b7d13c95e4b
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-11-08 20:25:53 -0600

    added constructor, getters/setters

--- src/main/java/com/projects/fullstackproject/controllers/Cart.java
+++ src/main/java/com/projects/fullstackproject/controllers/Cart.java
@@ -28,4 +28,62 @@ public class Cart {
 
     @Column(length = 10, nullable = false)
     private String phone;
+
+    public Cart(){}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public List<ServiceCatalog> getCartServices() {
+        return cartServices;
+    }
+
+    public void setCartServices(List<ServiceCatalog> cartServices) {
+        this.cartServices = cartServices;
+    }
+
+    public List<Components> getCartComponents() {
+        return cartComponents;
+    }
+
+    public void setCartComponents(List<Components> cartComponents) {
+        this.cartComponents = cartComponents;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
