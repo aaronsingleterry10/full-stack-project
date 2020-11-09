8f6967797d6f96eee36bf74cc61d8c72ecc7c862
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-11-08 20:25:05 -0600

    added properties to cart class

--- src/main/java/com/projects/fullstackproject/controllers/Cart.java
+++ src/main/java/com/projects/fullstackproject/controllers/Cart.java
@@ -1,4 +1,31 @@
 package com.projects.fullstackproject.controllers;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "cart")
 public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToMany
+    private List<ServiceCatalog> cartServices;
+
+    @OneToMany
+    private List<Components> cartComponents;
+
+    @Column(length = 20, nullable = false)
+    private String firstName;
+
+    @Column(length = 20, nullable = false)
+    private String lastName;
+
+    @Column(length = 50, nullable = false)
+    private String address;
+
+    @Column(length = 10, nullable = false)
+    private String phone;
 }
