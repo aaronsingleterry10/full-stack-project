8843059b7e413558b3a88c2e9f4ef49e1b5e5488
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-11-10 16:32:29 -0600

    added GetMapping

--- src/main/java/com/projects/fullstackproject/controllers/HomeController.java
+++ src/main/java/com/projects/fullstackproject/controllers/HomeController.java
@@ -1,4 +1,15 @@
 package com.projects.fullstackproject.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String landingPage(Model model) {
+        model.addAttribute("title", "Home");
+        return "home";
+    }
 }
