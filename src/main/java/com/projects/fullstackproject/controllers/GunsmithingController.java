c3ef99daba707b65a68f093efc3b2101d999c16a
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-11-11 21:51:27 -0600

    added getmapping to GunsmithingController

--- src/main/java/com/projects/fullstackproject/controllers/GunsmithingController.java
+++ src/main/java/com/projects/fullstackproject/controllers/GunsmithingController.java
@@ -1,4 +1,13 @@
 package com.projects.fullstackproject.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class GunsmithingController {
+
+    @GetMapping("/gunsmithing")
+    public String gunsmithingPage() {
+        return "gunsmithing";
+    }
 }
