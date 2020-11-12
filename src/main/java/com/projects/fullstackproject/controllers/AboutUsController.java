e837a339b89e78a9823db04bffdb2c889229c925
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-11-11 22:03:04 -0600

    added GetMapping

--- src/main/java/com/projects/fullstackproject/controllers/AboutUsController.java
+++ src/main/java/com/projects/fullstackproject/controllers/AboutUsController.java
@@ -1,4 +1,13 @@
 package com.projects.fullstackproject.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AboutUsController {
+
+    @GetMapping("/about-us")
+    public String aboutUsPage() {
+        return "about-us";
+    }
 }
