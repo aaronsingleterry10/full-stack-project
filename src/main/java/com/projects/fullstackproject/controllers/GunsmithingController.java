39f68fb1ca36940a015ad0454444a18fce5f00cf
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-11-14 08:29:03 -0600

    added get mapping to new views

--- src/main/java/com/projects/fullstackproject/controllers/GunsmithingController.java
+++ src/main/java/com/projects/fullstackproject/controllers/GunsmithingController.java
@@ -10,4 +10,19 @@ public class GunsmithingController {
     public String gunsmithingPage() {
         return "gunsmith-services";
     }
+
+    @GetMapping("/ar15")
+    public String ar15Services() {
+        return "gunsmith-services/ar15";
+    }
+
+    @GetMapping("/ak47")
+    public String ak47Services() {
+        return "gunsmith-services/ak47";
+    }
+
+    @GetMapping("/misc")
+    public String miscServices() {
+        return "gunsmith-services/misc";
+    }
 }
