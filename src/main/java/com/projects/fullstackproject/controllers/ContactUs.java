179168206184be0034d8306ab9d9ae07f67d3737
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-11-26 12:12:33 -0600

    added getmapping

--- src/main/java/com/projects/fullstackproject/controllers/ContactUs.java
+++ src/main/java/com/projects/fullstackproject/controllers/ContactUs.java
@@ -1,7 +1,20 @@
 package com.projects.fullstackproject.controllers;
 
+import com.projects.fullstackproject.services.EmailService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ContactUs {
+
+    private final EmailService emailService;
+
+    public ContactUs(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @GetMapping("/contact-us")
+    public String contactUsPage() {
+        return "contact-us";
+    }
 }
