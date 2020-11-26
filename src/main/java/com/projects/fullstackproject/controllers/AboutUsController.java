2443fb949ad3e03006afa5c8e52fd8446f2bcdb1
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-11-26 17:58:50 -0600

    deleted email service dependancy injenction

--- src/main/java/com/projects/fullstackproject/controllers/AboutUsController.java
+++ src/main/java/com/projects/fullstackproject/controllers/AboutUsController.java
@@ -7,15 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AboutUsController {
 
-    private final EmailService emailService;
-
-    public AboutUsController(EmailService emailService) {
-        this.emailService = emailService;
-    }
-
     @GetMapping("/about-us")
     public String aboutUsPage() {
-//        emailService.prepareAndSend("aaronsingleterry10@gmail.com", "this is a test", "Hello from About Us");
         return "about-us";
     }
 }
