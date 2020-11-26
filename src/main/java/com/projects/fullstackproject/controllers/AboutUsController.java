78f2ede17c2bff7df7819c48ad11f064062ca9f3
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-11-26 12:02:23 -0600

    tested the email service

--- src/main/java/com/projects/fullstackproject/controllers/AboutUsController.java
+++ src/main/java/com/projects/fullstackproject/controllers/AboutUsController.java
@@ -1,13 +1,21 @@
 package com.projects.fullstackproject.controllers;
 
+import com.projects.fullstackproject.services.EmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AboutUsController {
 
+    private final EmailService emailService;
+
+    public AboutUsController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
     @GetMapping("/about-us")
     public String aboutUsPage() {
+//        emailService.prepareAndSend("aaronsingleterry10@gmail.com", "this is a test", "Hello from About Us");
         return "about-us";
     }
 }
