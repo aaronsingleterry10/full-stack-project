ffe0ccb7db13a9022ab9aad52727b33d636ab458
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-11-26 11:34:45 -0600

    added to email service

--- src/main/java/com/projects/fullstackproject/services/EmailService.java
+++ src/main/java/com/projects/fullstackproject/services/EmailService.java
@@ -1,4 +1,35 @@
 package com.projects.fullstackproject.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service("mailService")
 public class EmailService {
+
+    @Autowired
+    public JavaMailSender emailSender;
+
+    @Value("aaronstrings@gmail.com")
+    private String from;
+
+    public void prepareAndSend(String to, String subject, String body) {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setFrom(from);
+        msg.setTo(to);
+        msg.setSubject(subject);
+        msg.setText(body);
+
+        try{
+            this.emailSender.send(msg);
+        }
+        catch (MailException ex) {
+            // simply log it and go on...
+            System.err.println(ex.getMessage());
+        }
+    }
+
 }
