3a351e374a7eb78b104d59a684dd45e795edf401
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-11-26 17:52:02 -0600

    added post mapping

--- src/main/java/com/projects/fullstackproject/controllers/ContactUs.java
+++ src/main/java/com/projects/fullstackproject/controllers/ContactUs.java
@@ -3,6 +3,8 @@ package com.projects.fullstackproject.controllers;
 import com.projects.fullstackproject.services.EmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ContactUs {
@@ -16,5 +18,14 @@ public class ContactUs {
     @GetMapping("/contact-us")
     public String contactUsPage() {
         return "contact-us";
+    }
+
+    @PostMapping("/contact-us")
+    public String messageSent(@RequestParam(name = "customer-name") String name,
+                              @RequestParam(name = "email") String email,
+                              @RequestParam(name = "phone") String phone,
+                              @RequestParam(name = "message") String message) {
+        emailService.prepareAndSend("aaronsingleterry10@gmail.com", "Question from customer", email + "\n" + phone + "\n" + message);
+        return "home";
     }
 }
